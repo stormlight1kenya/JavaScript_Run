3fd88ff6a0dbc102cd73883040aff32082e39452
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-01-20 16:32:17 +0300

    number guessing  game
    game created with math operators

---
+++ chap 1/chapq.js
@@ -0,0 +1,19 @@
+//playing a random number game
+console.log("Lats play a geme.Guess a number between 1 to 100");
+x=20;
+randomnum =Math.floor(Math.random()*100);
+console.log("Your Number is: "+ x);
+if (x == randomnum){
+
+console.log(randomnum + " Just the right number!");
+}
+if (x < randomnum){
+    console.log(randomnum + " Too Small!");
+}else
+if(x > randomnum){
+    console.log(randomnum + " Too big!");
+};
+
+console.log(/*"Emmanuel"*/);/* This is
+my name
+*/
