ceca8290bcf91d0034ee5a9fafed325a68602538
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-13 20:20:22 +0300

    speacial functions

--- chap 6/default.js
+++ chap 6/default.js
@@ -1,7 +1,15 @@
 
-
+console.log("Using Arrow Function");
 let petAnimals = ['Cat', 'Dog', 'Rabbit', 'Parrot', 'Squirel'];
- //petAnimals.forEach(pet => console.log(pet));
+ petAnimals.forEach(pet => console.log(pet+'\n'));
 
+console.log("Using Spread Function")
  let animalArray = ['Lion','Tiger',...petAnimals,'Puma','Bear','Wolve'];
-animalArray.forEach(animal => console.log(animal));
+animalArray.forEach(animal => console.log(animal+'\n'));
+
+console.log("Combing Arrow Function and Spread Function");
+let twoNumbers = [12,5];
+let addTwoNumbers = (numb1,numb2)=>{
+    console.log(numb1+numb2);
+}
+addTwoNumbers(...twoNumbers);
