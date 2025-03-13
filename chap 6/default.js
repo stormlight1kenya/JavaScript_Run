b7de0e3553b8e02ba418f24022ee6694360e1a68
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-13 20:07:34 +0300

    using special functions
    Understanding arrow and spread functions

---
+++ chap 6/default.js
@@ -0,0 +1,7 @@
+
+
+let petAnimals = ['Cat', 'Dog', 'Rabbit', 'Parrot', 'Squirel'];
+ //petAnimals.forEach(pet => console.log(pet));
+
+ let animalArray = ['Lion','Tiger',...petAnimals,'Puma','Bear','Wolve'];
+animalArray.forEach(animal => console.log(animal));
