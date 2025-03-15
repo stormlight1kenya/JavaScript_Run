9b4dba057800dc97f7716c5b02a372323b3cdde0
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-15 14:49:37 +0300

    time out callback

--- chap 6/callback.js
+++ chap 6/callback.js
@@ -6,3 +6,8 @@ function sayGoodbay(){
     console.log('And Goodbye');
 }
 greetFriend(`Ruth`, sayGoodbay);
+
+let youGotThis = function () {
+    console.log("You're doing really well, keep coding!");
+    };
+    setTimeout(youGotThis, 1000);
