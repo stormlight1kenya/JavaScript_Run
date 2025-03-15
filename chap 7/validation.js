b193b09038e74547673023a133b41289dce23f06
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-15 22:08:32 +0300

    validation with getters and setters

---
+++ chap 7/validation.js
@@ -0,0 +1,10 @@
+class Pepoel{
+    #name;
+    #address; 
+    constructor(name,address){
+        this.#name = name;
+        this.#address = address;
+    }
+}
+let pepoel1 =new Pepoel('Betty','Arizona SWJ 01');
+console.log(pepoel1.thisname);
