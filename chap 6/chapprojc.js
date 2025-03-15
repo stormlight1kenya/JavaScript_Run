5708fafae2cce2cf1dc061c6418e0ea12d9f6564
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-15 16:30:28 +0300

    set timeout order

--- chap 6/chapprojc.js
+++ chap 6/chapprojc.js
@@ -1,3 +1,5 @@
+//Count up recursive
+/*
 let number;
  function countRecursive(number){
     if(number <10){
@@ -12,3 +14,33 @@ let number;
  }
  console.log(countRecursive(11));
  console.log(countRecursive(2));
+ */
+
+ //set time out order
+
+let oneOut = ()=>{ console.log('One');
+        
+}
+
+let twoOut = ()=> console.log('Two');
+
+let threeout = ()=>{console.log('Three');
+    oneOut();
+    twoOut();
+
+}
+
+let fourOut = ()=>{ console.log('Four');
+    setTimeout(oneOut,0);
+    threeout();
+
+
+}
+console.log('First OutPut\n');
+oneOut();
+console.log('Second Output\n');
+twoOut();
+console.log('Third Output\n');
+threeout();
+console.log('Fourth Output\n');
+fourOut();
