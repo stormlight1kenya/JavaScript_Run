7892e987554e1081dbe7184472be250a8635f233
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-16 11:59:15 +0300

    validaion

--- chap 7/validation.js
+++ chap 7/validation.js
@@ -1,10 +1,15 @@
+//make city code satry with k
 class Pepoel{
     #name;
+    #city
     #address; 
-    constructor(name,address){
+    constructor(name,city,address){
         this.#name = name;
+       if(city.starsWith('K')){ this.#city = city;}else{
+        this.#city = 'K'+city;
+       }
         this.#address = address;
     }
 }
-let pepoel1 =new Pepoel('Betty','Arizona SWJ 01');
+let pepoel1 =new Pepoel('Betty','Jk1NO','Arizona SWJ 01');
 console.log(pepoel1.thisname);
