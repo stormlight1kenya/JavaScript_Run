65f79803ee6d5b7d18a2b0d161fc24550a79d107
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-16 22:11:13 +0300

    inheritance

---
+++ chap 7/inheriance.js
@@ -0,0 +1,45 @@
+//parent class
+class Vehicle {
+    constructor(color, currentSpeed, maxSpeed) {
+    this.color = color;
+    this.currentSpeed = currentSpeed;
+this.maxSpeed = maxSpeed;
+}
+move() {
+console.log("moving at", this.currentSpeed);
+}
+accelerate(amount) {
+this.currentSpeed += amount;
+}
+}
+
+//child class
+class Motorcycle extends Vehicle {
+    constructor(color, currentSpeed, maxSpeed, fuel) {
+    super(color, currentSpeed, maxSpeed);
+    this.fuel = fuel;
+    }
+    doWheelie() {
+    console.log("Driving on one wheel!");
+    }
+    }
+
+    let motor = new Motorcycle("Black", 0, 250, "gasoline");
+    console.log(motor.color);
+    motor.accelerate(50);
+    motor.move();
+
+    //second child class
+    class Boat extends Vehicle{
+        constructor(color,currentSpeed,maxSpeed,waterDiplacement){
+        super(color,currentSpeed,maxSpeed);
+        this.waterDiplacement = waterDiplacement;
+        floatWater(){
+            console.log('Hello there I am crusing on water');
+
+        }
+
+
+        }
+    }
+    let newVirginia = new Boat()y
