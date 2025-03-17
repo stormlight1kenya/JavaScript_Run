286b41545453f8444ea6496478d190158ade1252
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-17 15:22:38 +0300

    manipulating prototypes

--- chap 7/prototypes.js
+++ chap 7/prototypes.js
@@ -12,7 +12,12 @@ class Person {
 Person.prototype.introduce = function () {
     console.log("Hi, I'm", this.firstname);
     };
-    Person.prototype.favoriteColor = "green";
+    Person.prototype.favoriteColor = "green"; //this acts a default value
     let p = new Person("Maria", "Saga");
 console.log(p.favoriteColor);
 p.introduce();
+
+let man = new Person('James','Wachira');
+man.favoriteColor = 'blue';
+console.log(man.favoriteColor);
+man.introduce();
