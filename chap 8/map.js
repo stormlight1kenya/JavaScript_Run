1b138fa6788391faa3e7264505273177ef3ba3a5
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-28 19:59:17 +0300

    mapping an array
    map method affects the whole array

---
+++ chap 8/map.js
@@ -0,0 +1,7 @@
+let arrayOne = [1,2,3,4,5,6];
+let mapOne = arrayOne.map(x=> x - 1);
+console.log(mapOne);
+
+let arrayTwo = ['peter','paul','susan','robert','westbrige'];
+let mapTwo = arrayTwo.map(x => x.toUpperCase());
+console.log(mapTwo);
