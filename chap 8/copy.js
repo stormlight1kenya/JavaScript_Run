97b355740514aaca1e9b00395d9ef475d3cec956
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-28 19:42:57 +0300

    Update copy.js

--- chap 8/copy.js
+++ chap 8/copy.js
@@ -1,3 +1,8 @@
+/*The copywitthin has theree 
+The first values shows which index to replace
+The second on shows which index to be cope to replace the first value
+The third value reprents the range of copying 
+*/
 arr = ["grapefruit", 4, "hello", 5.6,11,'here', true];
 arr.copyWithin(0, 5,6);
 console.log(arr);
