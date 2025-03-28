95d0b666b15be6be74a5fbaca78d016cc88eaa5a
Emmanuel Sikuku <112975021+stormlight1kenya@users.noreply.github.com>
2025-03-28 19:47:31 +0300

    Update copy.js

--- chap 8/copy.js
+++ chap 8/copy.js
@@ -2,7 +2,12 @@
 The first values shows which index to replace
 The second on shows which index to be cope to replace the first value
 The third value reprents the range of copying 
+not specifyiny he range will copy all of the values 
 */
-arr = ["grapefruit", 4, "hello", 5.6,11,'here', true];
+let arr = ["grapefruit", 4, "hello", 5.6,11,'here', true];
 arr.copyWithin(0, 5,6);
 console.log(arr);
+
+let arr2 = ["grapefruit", 4, "hello there", 5.6,11,'here', true,'last'];
+arr2.copyWithin(0,5);
+console.log(arr2);
